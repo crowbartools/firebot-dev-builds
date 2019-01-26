74b0225fdeffcf85ec3d5592ecfc0bcb77a72c1c
SReject <sreject@comcast.net>
2019-01-25 22:59:35 -0500

    setup for travis build

--- copy.bat
+++
@@ -1,2 +0,0 @@
-echo %CD%
-xcopy .\Firebot\resources .\Firebot\dist\Firebot-win32-x64\resources /s /e /i /y
--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,33 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './Firebot/resources',
+                        src: ['**'],
+                        dest: 'Firebot/dist/Firebot-win32-x64/resources/',
+                        filter: 'isFile'
+                    }
+
+                ]
+            }
+        },
         zip_directories: {
-            files: [{
-                filter: 'isDirectory',
-                expand: true,
-                cmd: './firebot/dist',
-                src: ['*'],
-                dest: './firebot'
-            }]
+            firebot: {
+                files: [{
+                    expand: true,
+                    filter: 'isDirectory',
+                    cwd: './Firebot/dist/',
+                    src: ['*'],
+                    dest: ''
+                }]
+            }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-zip-directories');
+
+    grunt.registerTask('default', ['copy', 'zip_directories']);
 };
